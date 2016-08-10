dd8cd77460c08840ce4875325ed2f679fb77d672
Abdullah  Alshammari <abshammeri@gmail.com>
2016-08-10 20:48:06 +0300

    ticket auth added

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 var AlfrescoApi = require('alfresco-js-api');
 
-this.alfrescoJsApi = new AlfrescoApi({ username:'admin', password:'admin', host:'http://139.162.162.203'});
 
-this.alfrescoJsApi.login().then(function (data) {
-    console.log('API called successfully Login ticket:' + data);
+this.alfrescoJsApi = new AlfrescoApi({ ticket:'TICKET_1bcb6244507268b8f5247e7ea4f3371dd7caa30e', host:'http://139.162.162.203'});
+this.alfrescoJsApi.nodes.createFolder('newFolderNameX').then(function (data) {
+    console.log('The folder is created in root');    
 }, function (error) {
-    console.error(error);
+    console.log('Error in creation of this folder or folder already exist' + error);
 });
+
+// this.alfrescoJsApi.login().then(function (data) {
+//     console.log('API called successfully Login ticket:' + data);
+// }, function (error) {
+//     console.error(error);
+// });
 
