820171d79ac5b748bf1456b9ea6c888ab4c059d3
Abdullah  Alshammari <abshammeri@gmail.com>
2016-08-10 19:50:26 +0300

    login command added

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var AlfrescoApi = require('alfresco-js-api');
 
-this.alfrescoJsApi = new AlfrescoApi({ username:'admin', password:'admin', host:'http://127.0.0.1:8080'});
+this.alfrescoJsApi = new AlfrescoApi({ username:'admin', password:'admin', host:'http://139.162.162.203'});
 
 this.alfrescoJsApi.login().then(function (data) {
     console.log('API called successfully Login ticket:' + data);
